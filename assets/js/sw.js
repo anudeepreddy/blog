bc732b1bd4cefc8af3d244829965350c3a113e77
anudeepreddy <anudeepc85@gmail.com>
2017-12-26 14:33:30 +0530

    Update sw.js

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,76 +1,14 @@
-window.addEventListener('beforeinstallprompt', function(e) {
-  // beforeinstallprompt Event fired
-
-  // e.userChoice will return a Promise.
-  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
-  e.userChoice.then(function(choiceResult) {
-
-    console.log(choiceResult.outcome);
-
-    if(choiceResult.outcome == 'dismissed') {
-      console.log('User cancelled home screen install');
-    }
-    else {
-      console.log('User added to home screen');
-    }
-  });
+self.addEventListener('install', e => {
+ e.waitUntil(
+   // after the service worker is installed,
+   // open a new cache
+   caches.open('my-pwa-cache').then(cache => {
+     // add all URLs of resources we want to cache
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/about.html',    
+     ]);
+   })
+ );
 });
-
-var deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', function(e) {
-  console.log('beforeinstallprompt Event fired');
-  e.preventDefault();
-
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-
-  return false;
-});
-
-btnSave.addEventListener('click', function() {
-  if(deferredPrompt !== undefined) {
-    // The user has had a positive interaction with our app and Chrome
-    // has tried to prompt previously, so let's show the prompt.
-    deferredPrompt.prompt();
-
-    // Follow what the user has done with the prompt.
-    deferredPrompt.userChoice.then(function(choiceResult) {
-
-      console.log(choiceResult.outcome);
-
-      if(choiceResult.outcome == 'dismissed') {
-        console.log('User cancelled home screen install');
-      }
-      else {
-        console.log('User added to home screen');
-      }
-
-      // We no longer need the prompt.  Clear it up.
-      deferredPrompt = null;
-    });
-  }
-});
-var SLOW_TIME = 3000;
-
-this.addEventListener( 'install', function () {
-  console.log('Installed service worker');
-} );
-
-this.addEventListener( 'fetch', function(event) {
-  var url = event.request.url;
-
-  if ( url.indexOf( 'blocking' ) === -1) {
-  return;
-  }
-
-  var promise = Promise.race( [
-    new Promise( ( resolve, reject) => setTimeout(
-      () => reject( new Response( 'Request killed!' ) ),
-      SLOW_TIME
-    ) ),
-    fetch( event.request ),
-  ] );
-
-  event.respondWith( promise );
-} );
